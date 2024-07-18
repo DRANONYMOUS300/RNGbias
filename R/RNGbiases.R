f52fc39dfd596f21422841673bbdc6ca36e5a130
Martin Steppan <dranonymous300@gmail.com>
2024-07-18 17:45:07 +0200

    now

--- R/RNGbiases.R
+++ R/RNGbiases.R
@@ -129,7 +129,7 @@ RNGbiases <- function(data, mins, maxs, nsim = 10000, seed = 123456789) {
 
   }
 
-  biaspercZ$popularityZ <- scale(popperc)
+  biaspercZ[,8] <- scale(popperc)
 
   colnames(biaspercZ) <- c("primeavoidanceZ","evenZ","mult5Z","mult10Z","c0Z","c5Z","centralityZ","popularityZ")
 
